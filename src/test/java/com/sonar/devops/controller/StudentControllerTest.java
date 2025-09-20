b45bf79af4524460a98f2def866ec855c6431991
maheedhar <maheedhar.v04@gmail.com>
2025-09-20 23:00:51 +0530

    added test cases

--- src/test/java/com/sonar/devops/controller/StudentControllerTest.java
+++ src/test/java/com/sonar/devops/controller/StudentControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,7 +24,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Arrays;
 import java.util.List;
 
-@SpringBootTest
+
+@WebMvcTest(controllers = StudentController.class,excludeAutoConfiguration = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 @AutoConfigureMockMvc
 public class StudentControllerTest {
     @Autowired
@@ -36,33 +40,37 @@ public class StudentControllerTest {
     private ObjectMapper objectMapper;
 
     @Test
+    @WithMockUser(username = "Maheedhar", roles = {"USER"})
     public void testGetAllAvailableStudents() throws Exception {
         List<StudentDTO> students = Arrays.asList(new StudentDTO(1L, "Maheedhar", "CSE", "Hyderabad"),
                 new StudentDTO(2L, "Sai", "Mech", "Hyderabad"));
         Mockito.when(studentService.findAllStudents()).thenReturn(students);
-        mockMvc.perform(get("/students").with(SecurityMockMvcRequestPostProcessors.httpBasic("Maheedhar","password")))
+        mockMvc.perform(get("/students"))
+                        //.with(SecurityMockMvcRequestPostProcessors.httpBasic("Maheedhar","password")))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()").value(2))
                 .andExpect(jsonPath("$[0].name").value("Maheedhar"));
     }
 
     @Test
+    @WithMockUser(username = "Maheedhar", roles = {"USER"})
     public void testGetStudentById() throws Exception {
         StudentDTO student = new StudentDTO(1L, "Maheedhar", "CSE", "Hyderabad");
         Mockito.when(studentService.findById(1L)).thenReturn(student);
-        mockMvc.perform(MockMvcRequestBuilders.get("/students/1").with(SecurityMockMvcRequestPostProcessors.httpBasic("Maheedhar","password")))
+        mockMvc.perform(MockMvcRequestBuilders.get("/students/1"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").value("Maheedhar"));
     }
 
     @Test
+    @WithMockUser(username = "Admin", roles = {"ADMIN"})
     public void testSearchStudentsByIds() throws Exception {
         List<StudentDTO> students = List.of(
                 new StudentDTO(1L, "John Doe", "Computer Science", "New York")
         );
         Mockito.when(studentService.getAllStudentsByIds(Arrays.asList(1L, 2L))).thenReturn(students);
         mockMvc.perform(MockMvcRequestBuilders.post("/students/search")
-                        .with(SecurityMockMvcRequestPostProcessors.httpBasic("Admin","admin"))
+                        //.with(SecurityMockMvcRequestPostProcessors.httpBasic("Admin","admin"))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(Arrays.asList(1L, 2L))))
                 .andExpect(status().isOk())
@@ -71,12 +79,13 @@ public class StudentControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "Admin", roles = {"ADMIN"})
     public void testSaveStudent() throws Exception {
         StudentDTO input = new StudentDTO(null, "Maheedhar", "CS", "Hyderabad");
         StudentDTO saved = new StudentDTO(1L, "Maheedhar", "CSE", "Hyderabad");
         Mockito.when(studentService.saveStudent(Mockito.any(StudentDTO.class))).thenReturn(saved);
         mockMvc.perform(post("/students/save")
-                        .with(SecurityMockMvcRequestPostProcessors.httpBasic("Admin","admin"))
+                        //.with(SecurityMockMvcRequestPostProcessors.httpBasic("Admin","admin"))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(input)))
                 .andExpect(status().isOk())
@@ -84,6 +93,7 @@ public class StudentControllerTest {
     }
 
     @Test
+    @WithMockUser(username = "Admin", roles = {"ADMIN"})
     public void testSaveStudents() throws Exception {
 
     List<StudentDTO> input = List.of(new StudentDTO(null, "Maheedhar", "CSE", "Hyderabad"),
@@ -92,7 +102,6 @@ public class StudentControllerTest {
             new StudentDTO(2L, "Ravi", "IT", "Banglore"));
     Mockito.when(studentService.saveAllStudents(Mockito.anyList())).thenReturn(saved);
     mockMvc.perform(post("/students/saveall")
-                    .with(SecurityMockMvcRequestPostProcessors.httpBasic("Admin","admin"))
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsString(input)))
             .andExpect(status().isOk())
